985b32a60e8928dfa33d0756018853a95693f129
VaulinDI <vdi85@mail.ru>
2022-09-04 18:48:27 +0300

    sumdigits task solved

---
+++ Example04_SumDigits/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму цифр числа
+int sum = 0;
+int Number(int n) 
+{
+    if (n != 0) 
+    { 
+        sum = sum + n % 10;
+        Number(n / 10); 
+    } 
+return sum;
+}
+
+int n = 10340;
+Console.WriteLine(Number(n));
