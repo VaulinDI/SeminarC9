25773d29f8c3e67462024d18c5b48ee5de1014a1
VaulinDI <vdi85@mail.ru>
2022-09-04 17:59:25 +0300

    natural task solved

---
+++ Example01_Natural/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void Number(int n) 
+{ 
+    if (1 <= n) 
+    { 
+    Console.Write("{0}", n); 
+    Number(n-1); 
+    } 
+}
+
+int n = 5;
+Number(n);
+Console.WriteLine();
