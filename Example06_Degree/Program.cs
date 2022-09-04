bf98b0f99e1fece182082cd7795d72bd60dc57a9
VaulinDI <vdi85@mail.ru>
2022-09-04 19:33:15 +0300

    degree task solved

---
+++ Example06_Degree/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу возведения числа А в целую стень B
+int result = 1;
+int Number(int n, int m, int x) 
+{
+    if (x <= m) 
+    { 
+        result = result * n;
+        Number(n, m, x + 1); 
+    } 
+return result;
+}
+
+int n = 10;
+int m = 3;
+int x = 1;
+Console.WriteLine(Number(n, m, x));
