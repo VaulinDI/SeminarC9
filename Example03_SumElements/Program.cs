5b46c4a7660670492ceae2873e99f5bd161859a8
VaulinDI <vdi85@mail.ru>
2022-09-04 18:23:07 +0300

    sumelements task solved

---
+++ Example03_SumElements/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int sum = 0;
+int Number(int m, int n) 
+{
+    if (m <= n) 
+    { 
+        sum = sum + m;
+        Number(m + 1, n); 
+    } 
+return sum;
+}
+
+int m = 1;
+int n = 5;
+Console.WriteLine(Number(m, n));
