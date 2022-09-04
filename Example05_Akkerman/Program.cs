b2020dc204c3836269a4ff8b6c67a5cae06f3974
VaulinDI <vdi85@mail.ru>
2022-09-04 19:20:08 +0300

    akkerman task solved

---
+++ Example05_Akkerman/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу вычисления функции Аккермана
+int A(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else 
+        if ((n != 0) && (m == 0))
+        {
+            return A(n - 1, 1);
+        }
+        else
+        {
+            return A(n - 1, A(n, m - 1));
+        }
+}
+
+int n = 1;
+int m = 2;
+Console.WriteLine(A(n, m));
