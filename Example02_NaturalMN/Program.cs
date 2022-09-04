91b0359cf7426975252406bcded66dd0d909b261
VaulinDI <vdi85@mail.ru>
2022-09-04 18:06:00 +0300

    naturalmn task solved

---
+++ Example02_NaturalMN/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void Number(int m, int n) 
+{ 
+    if (m <= n) 
+    { 
+    Console.Write(m + "  "); 
+    Number(m + 1, n); 
+    } 
+}
+
+int m = 3;
+int n = 9;
+Number(m, n);
+Console.WriteLine();
